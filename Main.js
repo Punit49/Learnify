95a254b412704252ee8096825452a374d75b27c8
Punit49 <therealpunit5@gmail.com>
2025-03-17 14:32:21 +0530

    Course card section add with smooth dyanmic popups

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 const courseTags = Object.keys(courseData);
 const coursesTab = document.querySelector(".courses-Tab");
+const imagePath = "Premium.png";
 
 // For Creating course category tab
 const createTag = (title) => {
@@ -47,7 +48,7 @@ courseCategory.forEach(category => {
     })
 });
 
-// Generating course card -
+// Generating course card and popup-
 const generateCourseCard = (course, parent) => {
     const newCourse = document.createElement("div");
     newCourse.classList.add("courses");
@@ -64,54 +65,127 @@ const generateCourseCard = (course, parent) => {
             </div>   
 
             <div class="course-Card-Popup">
-                            
+                           
             </div>
-        `
+        `;
 
     newCourse.addEventListener("mouseenter", (event) => {
         // Adding Hover eventListener to show preview -
-        const courseCardPopup = newCourse.querySelector(".course-Card-Popup");
+        const courseCardPopup = newCourse.querySelector(".course-Card-Popup") || 0;
 
         if(courseCardPopup){
+            // Adding Details in popup -
+            courseCardPopup.innerHTML = `
+                <div class="insideCoursePopup">
+                    <h5 class="course-Pop-Heading">
+                        ${course.title}
+                    </h5>
+                    <div class="course-Pop-Subhead">
+                        <div class="preBadge"> 
+                            <img src="Premium.png" alt="Premium">
+                            <span>Premium</span>
+                        </div>
+                    </div>
+                    <div class="course-Pop-body">
+                        <div class="popDescription">
+                            ${course.description}
+                        </div>
+                        <div class="WYLHead">
+                            What You'll Learn
+                        </div>
+                        <div class="popWYL">
+                            <ul>
+                                <li>${course.what_you_will_learn[0]}</li>
+                                <li>${course.what_you_will_learn[1]}</li>
+                                <li>${course.what_you_will_learn[2]}</li>   
+                            </ul>
+                        </div>
+                    </div>
+                    <button class="addPopupCart">
+                        Add to cart
+                    </button>
+                </div>
+            `;
+
+            // Showing and deciding place for popup -
             const rect = newCourse.getBoundingClientRect(); 
             const viewportWidth = window.innerWidth;
             const popupWidth = courseCardPopup.offsetWidth || 200;
 
-            // courseCardPopup.style.display = "block";
-            // courseCardPopup.style.visibility = "hidden"; 
-
             let leftPosition;
+            let coursePadding;
 
             if(rect.right + popupWidth + 10 > viewportWidth){
-                leftPosition = rect.left + window.scrollX - popupWidth - 240;
+                coursePadding = "paddingRight";
+                leftPosition = rect.left + window.scrollX - popupWidth - 95;
             }
             else {
-                leftPosition = rect.left + window.scrollX + rect.width - 80;
+                coursePadding = "paddingLeft";
+                leftPosition = rect.left + window.scrollX + rect.width - 100;
             }
             
+            courseCardPopup.style[coursePadding] = "1rem";
             courseCardPopup.style.left = `${leftPosition}px`;
-            courseCardPopup.style.display = "block";
+            courseCardPopup.style.opacity = "1";
             courseCardPopup.style.visibility = "visible";
+            courseCardPopup.style.transform = "translateY(0)";
         }      
     });
 
     newCourse.addEventListener("mouseleave", (event) => {
-        const courseCardPopup = newCourse.querySelector(".course-Card-Popup");
+        const courseCardPopup = newCourse.querySelector(".course-Card-Popup") || 0;
         if(courseCardPopup){
-            courseCardPopup.style.display = "none";
+            setTimeout(() => {
+                courseCardPopup.style.opacity = "0";
+                courseCardPopup.style.visibility = "hidden";
+                courseCardPopup.style.transform = "translateY(10px)";
+            }, 100)
+        }
+    }); 
+    
+    const courseCardPopup = newCourse.querySelector(".course-Card-Popup") || 0;
+    courseCardPopup.addEventListener("mouseleave", (event) => {
+        if(courseCardPopup){
+            setTimeout(() => {
+                courseCardPopup.style.opacity = "0";
+                courseCardPopup.style.visibility = "hidden";
+                courseCardPopup.style.transform = "translateY(10px)";
+            }, 100)
         }
     });
-    
-    // const courseCardPopup = newCourse.querySelector(".course-Card-Popup") || 0;
-    // courseCardPopup.addEventListener("mouseleave", (event) => {
-    //     if(courseCardPopup){
-    //         courseCardPopup.style.display = "none";
-    //     }
-    // });
-    
+
     // Adding to Parent -
     parent.appendChild(newCourse);
 }
+
+// Restricting mouse scroll -
+courseWrapper.addEventListener("wheel", (event) => event.preventDefault(), { passive: false });
+courseWrapper.addEventListener("touchmove", (event) => event.preventDefault(), { passive: false });
+
+// Scroll Button functionality -
+const courseCardLeftBtn = document.querySelector("#course-tab-right");
+const courseCardRightBtn = document.querySelector("#course-tab-left");
+const courseTagLeftBtn = document.querySelector("#courseCatLeft");
+const courseTagRightBtn = document.querySelector("#courseCatRight");
+
+
+// For course cards -
+courseCardRightBtn.addEventListener("click", () => {
+    courseWrapper.scrollLeft += 330;
+});
+
+courseCardLeftBtn.addEventListener("click", () => {
+    courseWrapper.scrollLeft -= 330;
+});
+
+// For course tags -
+courseTagRightBtn.addEventListener("click", () => {
+    coursesTab.scrollLeft -= 270;
+});
+
+courseTagLeftBtn.addEventListener("click", () => {
+    coursesTab.scrollLeft += 270;
+});
 
 // Window Load Event -
 window.addEventListener("load", () => {
@@ -124,3 +198,4 @@ window.addEventListener("load", () => {
     courseCategory[0].classList.add("tabActive");
     courseCategory[0].classList.remove("course-tag-hover");
 });
+
